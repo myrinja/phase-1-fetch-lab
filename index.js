3aa9fcc279db8116243e5e932a0ae5621b303735
myrinja <myrinjau98@gmail.com>
2023-07-22 23:14:09 +0300

    fetch json data

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+   return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((books) => renderBooks(books))
+  
+  
 }
+//fetchBooks(books);
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +19,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks(books);
 });
